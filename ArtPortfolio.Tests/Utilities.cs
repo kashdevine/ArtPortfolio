2291fc64031665dd2d99db195812b33132132894
kashdevine <kasheen.mowatt@gmail.com>
2022-03-18 11:58:07 -0700

    Added the test project and added properties to the image and video models.

---
+++ ArtPortfolio.Tests/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using ArtPortfolio.Data;
+using ArtPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtPortfolio.Tests
+{
+    public static class Utilities
+    {
+        public static readonly string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=ArtPortfolioDbTest;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public static async Task SeedDbAsync(ArtPortfolioDbContext ctx)
+        {
+            await ctx.ProjectImages!.AddRangeAsync(SeedProjectImges());
+
+            await ctx.SaveChangesAsync();
+        }
+
+        public static async Task ReInitializeTestDb(ArtPortfolioDbContext ctx)
+        {
+            if (!await ctx.Database.CanConnectAsync()) {
+                await ctx.Database.EnsureCreatedAsync();
+            }
+
+            ctx.Remove(ctx.ProjectImages);
+
+            await SeedDbAsync(ctx);
+
+        }
+
+        public static List<ProjectImage> SeedProjectImges()
+        {
+            return new List<ProjectImage>()
+            {
+                new ProjectImage() { Name = "Image1", AltTitle="Image1AltTitle", Description="Crazy Image 1"},
+                new ProjectImage() { Name = "Image2", AltTitle="Image2AltTitle", Description="Crazy Image 2"},
+                new ProjectImage() { Name = "Image3", AltTitle="Image3AltTitle", Description="Crazy Image 3"},
+                new ProjectImage() { Name = "Image4", AltTitle="Image4AltTitle", Description="Crazy Image 4"},
+            };
+        }
+    }
+}
--- Data/ArtPortfolioDbContext.cs
+++ Data/ArtPortfolioDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ArtPortfolio.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtPortfolio.Data
 {
@@ -6,6 +7,30 @@ namespace ArtPortfolio.Data
     {
         public ArtPortfolioDbContext(DbContextOptions<ArtPortfolioDbContext> options) : base(options)
         {
+        }
+
+        public DbSet<ProjectImage>? ProjectImages { get; set; }
+        public DbSet<ProjectVideo>? ProjectVideos { get; set; }
+        public DbSet<Project>? Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasMany<ProjectVideo>()
+                .WithOne();
+            modelBuilder.Entity<Project>()
+                .HasMany<ProjectImage>()
+                .WithOne();
+
+            modelBuilder.Entity<ProjectVideo>()
+                .HasOne<Project>()
+                .WithMany()
+                .HasForeignKey(p=> p.ProjectId);
+
+            modelBuilder.Entity<ProjectImage>()
+                .HasOne<Project>()
+                .WithMany()
+                .HasForeignKey(p=> p.ProjectId);
         }
     }
 }
