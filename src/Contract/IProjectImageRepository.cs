f44e10969b9f992ac445519be7988fcfdfc8ff65
kashdevine <kasheen.mowatt@gmail.com>
2022-03-19 23:52:17 -0700

    Commented the first method and interface.

--- src/Contract/IProjectImageRepository.cs
+++ src/Contract/IProjectImageRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ArtPortfolio.Contract
+﻿using ArtPortfolio.Models;
+
+namespace ArtPortfolio.Contract
 {
+    /// <summary>
+    /// Interface for managing project images.
+    /// </summary>
     public interface IProjectImageRepository
     {
+        /// <summary>
+        /// Gets a list of project images from the DB.
+        /// </summary>
+        /// <returns>A list of project images.</returns>
+        public Task<IEnumerable<ProjectImage>> GetImagesAsync();
     }
 }
