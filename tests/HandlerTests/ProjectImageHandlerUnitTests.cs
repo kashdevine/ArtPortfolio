51c46c0ce4abc5ee2ed5818efd3b900503f3d0f2
kashdevine <kasheen.mowatt@gmail.com>
2022-03-23 23:46:54 -0700

    Created the project image handler test class.

---
+++ tests/HandlerTests/ProjectImageHandlerUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using ArtPortfolio.Handlers.ProjectImageHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArtPortfolio.Tests.HandlerTests
+{
+    [Collection("PortfolioTests")]
+    public class ProjectImageHandlerUnitTests
+    {
+        private IProjectImageHandler _sut;
+    }
+}
