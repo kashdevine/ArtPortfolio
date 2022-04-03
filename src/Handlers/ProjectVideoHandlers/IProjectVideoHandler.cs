0783362d52845a5182faafafb27115956a6b47f5
kashdevine <kasheen.mowatt@gmail.com>
2022-04-02 22:42:42 -0700

    Created the video handler interface and unit test class.

---
+++ src/Handlers/ProjectVideoHandlers/IProjectVideoHandler.cs
@@ -0,0 +1,21 @@
+﻿using ArtPortfolio.Models;
+
+namespace ArtPortfolio.Handlers.ProjectVideoHandlers
+{
+    public interface IProjectVideoHandler
+    {
+        /// <summary>
+        /// Saves a video to the disk.
+        /// </summary>
+        /// <param name="file">IFormfile object.</param>
+        /// <param name="video">ProjectVideo object.</param>
+        /// <returns>A boolean.</returns>
+        public bool SaveVideo(IFormFile file, ProjectVideo video);
+        /// <summary>
+        /// Deletes a video from the disk.
+        /// </summary>
+        /// <param name="video">ProjectVideo object.</param>
+        /// <returns>A boolean.</returns>
+        public bool DeleteVideo(ProjectVideo video);
+    }
+}
