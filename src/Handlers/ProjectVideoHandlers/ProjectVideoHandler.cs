7efaec4847369e7b5cb1962e8233adddbeac4fcd
kashdevine <kasheen.mowatt@gmail.com>
2022-04-03 19:50:21 -0700

    Created the handler class and updated the project video handler unit test.

---
+++ src/Handlers/ProjectVideoHandlers/ProjectVideoHandler.cs
@@ -0,0 +1,24 @@
+﻿using ArtPortfolio.Models;
+
+namespace ArtPortfolio.Handlers.ProjectVideoHandlers
+{
+    public class ProjectVideoHandler : IProjectVideoHandler
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _config;
+        public ProjectVideoHandler(IWebHostEnvironment env, IConfiguration config)
+        {
+            _env = env;
+            _config = config;
+        }
+        public bool DeleteVideo(ProjectVideo video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveVideo(IFormFile file, ProjectVideo video)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
