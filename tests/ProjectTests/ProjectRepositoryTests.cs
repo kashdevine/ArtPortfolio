5c3016a015132d2a11c301127a592559aafe5cda
kashdevine <kasheen.mowatt@gmail.com>
2022-04-06 23:55:47 -0700

    Created the project repo and unit test class.

---
+++ tests/ProjectTests/ProjectRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using ArtPortfolio.Contracts;
+using ArtPortfolio.Data;
+using ArtPortfolio.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtPortfolio.Tests.ProjectTests
+{
+    public class ProjectRepositoryTests
+    {
+        private ArtPortfolioDbContext _ctx;
+        private IProjectRepository _sut;
+
+        public ProjectRepositoryTests()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ArtPortfolioDbContext>()
+                    .UseSqlServer(connectionString: Utilities.ConnectionString)
+                    .Options;
+            _ctx = new ArtPortfolioDbContext(dbContextOptions);
+            _sut = new ProjectRepository(_ctx);
+        }
+    }
+}
