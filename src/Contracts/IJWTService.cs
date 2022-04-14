41763be5be72fbfa3c4d0c7e24d74aa4f022eb9d
kashdevine <kasheen.mowatt@gmail.com>
2022-04-13 21:34:16 -0700

    Updated the settings to have an admin secret and created the JWT service interface.

---
+++ src/Contracts/IJWTService.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtPortfolio.Contracts
+{
+    using System.Security.Claims;
+
+    public interface IJWTService
+    {
+        public string GetAccessToken(Claim[] claims);
+        public ClaimsPrincipal VerifyToken(string token);
+        public string GetRefreshtoken();
+    }
+}
