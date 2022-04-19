6248f622516c368b22c45f3964b6e001d993d75c
kashdevine <kasheen.mowatt@gmail.com>
2022-04-18 20:26:20 -0700

    Added configurations for the user, and role manager using identity api, and updated a line in the image handler.

---
+++ src/Data/ArtPortfolioUserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ArtPortfolio.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtPortfolio.Data
+{
+    public class ArtPortfolioUserDbContext : IdentityDbContext
+    {
+        public ArtPortfolioUserDbContext(DbContextOptions<ArtPortfolioUserDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProjectUser> Users { get; set; }
+        public DbSet<ProjectUserRole> Roles { get; set; }
+
+    }
+}
