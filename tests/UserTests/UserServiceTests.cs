5b4c8539713893683143ecd9897b77a7a0eb2aa1
kashdevine <kasheen.mowatt@gmail.com>
2022-04-20 22:52:40 -0700

    Set up the test environment and created the service class

--- tests/UserTests/UserServiceTests.cs
+++ tests/UserTests/UserServiceTests.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using ArtPortfolio.Contracts;
+using ArtPortfolio.Data;
+using ArtPortfolio.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
+using ArtPortfolio.Services;
 
 namespace ArtPortfolio.Tests.UserTests
 {
     public class UserServiceTests
     {
+        private ArtPortfolioUserDbContext _ctx;
+        private IUserService _sut;
+        private Mock<UserManager<ProjectUser>> _mockUserManager;
+        private Mock<RoleManager<ProjectUserRole>> _mockRoleManager;
+
         public UserServiceTests()
         {
+            var dbContextOptions = new DbContextOptionsBuilder<ArtPortfolioUserDbContext>()
+                     .UseSqlServer(connectionString: Utilities.ConnectionString)
+                     .Options;
+            
+            _mockUserManager = new Mock<UserManager<ProjectUser>>();
+            _mockRoleManager = new Mock<RoleManager<ProjectUserRole>>();
 
+            _ctx = new ArtPortfolioUserDbContext(dbContextOptions);
+            _sut = new UserService(_mockUserManager.Object, _mockRoleManager.Object);
         }
     }
 }
