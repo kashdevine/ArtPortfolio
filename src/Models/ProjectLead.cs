68d1efe5a0c1de4976042526acff03ef02bb8462
kashdevine <kasheen.mowatt@gmail.com>
2022-04-29 22:40:12 -0700

    Creaded the lead model to collect contact info on the frontend.

---
+++ src/Models/ProjectLead.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtPortfolio.Models
+{
+    public class ProjectLead
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(25, ErrorMessage = "First Name can only be up to 25 characters.")]
+        public string FirstName { get; set; }
+
+        [MaxLength(30, ErrorMessage = "Last Name can only be up to 25 characters.")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public bool IsSpam { get; set; }
+        public bool ForwardedToEmail { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
+
