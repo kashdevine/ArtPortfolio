e7d14d1fa87f72f22078251ce92ba5e8f7ba2604
kashdevine <kasheen.mowatt@gmail.com>
2022-04-30 23:34:04 -0700

    Created the project lead repo, and the repo test class

---
+++ src/Contracts/IProjectLeadRepository.cs
@@ -0,0 +1,46 @@
+﻿using ArtPortfolio.Models;
+
+namespace ArtPortfolio.Contracts
+{
+    public interface IProjectLeadRepository
+    {
+        /// <summary>
+        /// Get all the leads.
+        /// </summary>
+        /// <param name="token">Cancellation token.</param>
+        /// <returns>IEnumerable of project leads.</returns>
+        public Task<IEnumerable<ProjectLead>> GetAllLeads(CancellationToken token = default);
+
+        /// <summary>
+        /// Gets a lead based on the id.
+        /// </summary>
+        /// <param name="id">A Guid.</param>
+        /// <param name="token">Cancellation token.</param>
+        /// <returns>A project lead</returns>
+        public Task<ProjectLead> GetLeadById(Guid id, CancellationToken token = default);
+
+        /// <summary>
+        /// Creates a new project lead
+        /// </summary>
+        /// <param name="projectLead">A Project Lead</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>Newly created project lead</returns>
+        public Task<ProjectLead> CreateLead(ProjectLead projectLead, CancellationToken token = default);
+
+        /// <summary>
+        /// Updates an existing project lead
+        /// </summary>
+        /// <param name="projectLead">A Project Lead</param>
+        /// <param name="token">Cancellation Token</param>
+        /// <returns>Updated project lead</returns>
+        public Task<ProjectLead> UpdateLead(ProjectLead projectLead, CancellationToken token = default);
+
+        /// <summary>
+        /// Deletes a project based on the id.
+        /// </summary>
+        /// <param name="id">A Guid</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>A bool</returns>
+        public Task<bool> DeleteLead(Guid id, CancellationToken token = default);
+    }
+}
---
+++ src/Services/ProjectLeadRepository.cs
@@ -0,0 +1,43 @@
+﻿using ArtPortfolio.Contracts;
+using ArtPortfolio.Data;
+using ArtPortfolio.Models;
+
+namespace ArtPortfolio.Services
+{
+    /// <summary>
+    /// <inheritdoc cref="IProjectLeadRepository"/>
+    /// </summary>
+    public class ProjectLeadRepository : IProjectLeadRepository
+    {
+        private readonly ArtPortfolioDbContext _ctx;
+
+        public ProjectLeadRepository(ArtPortfolioDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public Task<ProjectLead> CreateLead(ProjectLead projectLead, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteLead(Guid id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProjectLead>> GetAllLeads(CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectLead> GetLeadById(Guid id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectLead> UpdateLead(ProjectLead projectLead, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
