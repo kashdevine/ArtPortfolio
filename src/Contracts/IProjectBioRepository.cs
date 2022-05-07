7a6bc75982a0ffe44fdc6d35607ab9fa1bef62d8
kashdevine <kasheen.mowatt@gmail.com>
2022-05-06 23:40:37 -0700

    Updated interface and implementation with token default and created test and method for creating bio.

--- src/Contracts/IProjectBioRepository.cs
+++ src/Contracts/IProjectBioRepository.cs
@@ -9,34 +9,34 @@ namespace ArtPortfolio.Contracts
         /// </summary>
         /// <param name="token">Cancellation token</param>
         /// <returns>A list of biographies</returns>
-        public Task<IEnumerable<ProjectBiography>> GetAllBios(CancellationToken token);
+        public Task<IEnumerable<ProjectBiography>> GetAllBios(CancellationToken token = default);
         /// <summary>
         /// Gets a bio by the id.
         /// </summary>
         /// <param name="id">A Guid</param>
         /// <param name="token">Cancellation token</param>
         /// <returns>Specified bio</returns>
-        public Task<ProjectBiography> GetBioById(Guid id, CancellationToken token);
+        public Task<ProjectBiography> GetBioById(Guid id, CancellationToken token = default);
         /// <summary>
         /// Creates a new bio
         /// </summary>
         /// <param name="bio">A ProjectBiography model</param>
         /// <param name="token">Cancellation token</param>
         /// <returns>Newly created bio</returns>
-        public Task<ProjectBiography> CreateBio(ProjectBiography bio, CancellationToken token);
+        public Task<ProjectBiography> CreateBio(ProjectBiography bio, CancellationToken token = default);
         /// <summary>
         /// Updates an existing bio
         /// </summary>
         /// <param name="bio">ProjectBiography model</param>
         /// <param name="token">Cancellation token</param>
         /// <returns>The updated bio.</returns>
-        public Task<ProjectBiography> UpdateBio(ProjectBiography bio, CancellationToken token);
+        public Task<ProjectBiography> UpdateBio(ProjectBiography bio, CancellationToken token = default);
         /// <summary>
         /// Deletes an existing bio
         /// </summary>
         /// <param name="id">A Guid</param>
         /// <param name="token">Cancellation token</param>
         /// <returns>A boolean</returns>
-        public Task<bool> DeleteBio(Guid id, CancellationToken token);
+        public Task<bool> DeleteBio(Guid id, CancellationToken token = default);
     }
 }
